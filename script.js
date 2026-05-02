6be55d8c72f8c2e30dbbf000a014ccc4c446416a
João Gabriel <jgabrielgm1@gmail.com>
2026-05-01 21:19:30 -0300

    Ajuste no horário do evento

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const EVENT_DATE_TEXT = "Evento gratuito! Dia 16 de maio de 2026, das 11h \u00e0s 12h - via Zoom";
+const EVENT_DATE_TEXT = "Evento gratuito! Dia 16 de maio de 2026, \u00e0s 11h - via Zoom";
 
 function syncEventDate() {
   const eventDateElements = document.querySelectorAll("[data-event-date]");
